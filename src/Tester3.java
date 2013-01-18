b4c9c242189cd4a36690b36867a0af386e1440b8
Colin Davis <colindavis02@gmail.com>
2013-01-18 09:48:35 -0500

    added convertDate(String date) Method

--- src/Tester3.java
+++ src/Tester3.java
@@ -29,19 +29,18 @@ public class Tester3 {
             while (rs.next()) {
                 System.out.println("ID: "+rs.getInt(2));		//i.e 1= first ever, 2 is second ever, etc. (1,3,4 happens)
                 System.out.println("User: "+rs.getInt(3));		//identifier for person
-                String rawDate= rs.getString(3);
                 System.out.println("Text: "+rs.getString(4));
-                System.out.println("Date: "+rs.getString(1));
+                System.out.println("Date: "+convertDate(rs.getString(1)));
                 System.out.println();
             }
             
             pst = con.prepareStatement("SELECT * FROM Users");
             rs = pst.executeQuery();
             
-    /*        while(rs.next()) {
+            while(rs.next()) {
             	System.out.println(rs.getString(2));
-            }
-*/
+           }
+
 
         } catch (SQLException ex) {
                 Logger lgr = Logger.getLogger(Tester3.class.getName());
@@ -65,6 +64,37 @@ public class Tester3 {
                 lgr.log(Level.WARNING, ex.getMessage(), ex);
             }
         }
+    }
+    private static String convertDate(String date)
+    {
+    	//Date: 2012-12-20 09:53:03.0
+    	//      YYYY-MM-DD HH:MM:SS.x
+    	//		0123-56-89 12:45:78
+    	//		HH:MM, MM/DD/YYYY
+    	//		12345, 56/89/0123
+    	char[] cDate = date.toCharArray();
+    	date = "";
+    	
+    	if(cDate[11] !='0')
+    		date+= cDate[11];
+    	date+= cDate[12];
+    	date+= cDate[13];
+    	date+= cDate[14];
+    	date+= cDate[15];
+    	date += ", ";
+    	if(cDate[5] !='0')
+    		date+= cDate[5];
+    	date+= cDate[6];
+    	date+= "/";
+    	if(cDate[8] !='0')
+    		date+= cDate[8];
+    	date+= cDate[9];
+    	date+= "/";
+    	date+= cDate[0];
+    	date+= cDate[1];
+    	date+= cDate[2];
+    	date+= cDate[3];
+    	return date;
     }
 }
 
